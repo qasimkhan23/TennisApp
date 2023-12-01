571f1dcd63a6478d4c11a0adc631bdfc08e95765
qasim-ssi <qkhan@ssidecisions.com>
2023-12-01 16:42:40 +0500

    fix: fixed image issues

--- src/screens/events/Events.tsx
+++ src/screens/events/Events.tsx
@@ -7,28 +7,28 @@ const EventsScreen = ({ navigation }: any) => {
   const data = [
     {
       profileImageSrc: require("../../../assets/profileImage.png"),
-      bgImageSource: require("../../../assets/bg_7.png"),
+      bgImageSource: require("../../../assets/bg_7.jpeg"),
       name: "Rona Frishman dfsfsfsdfdsfsdfsfsfsdfsfsdf",
       exp: "| Fearless",
       desp: "Ramat aliau, Rishon Le Zion, 20.01.2020 | 20:00",
     },
     {
       profileImageSrc: require("../../../assets/profileImage.png"),
-      bgImageSource: require("../../../assets/bg_7.png"),
+      bgImageSource: require("../../../assets/bg_8.jpeg"),
       name: "Rona Frishman |",
       exp: "Fearless",
       desp: "Ramat aliau, Rishon Le Zion, 20.01.2020 | 20:00",
     },
     {
       profileImageSrc: require("../../../assets/profileImage.png"),
-      bgImageSource: require("../../../assets/bg_7.png"),
+      bgImageSource: require("../../../assets/bg_9.jpeg"),
       name: "Rona Frishman |",
       exp: "Fearless",
       desp: "Ramat aliau, Rishon Le Zion, 20.01.2020 | 20:00",
     },
     {
       profileImageSrc: require("../../../assets/profileImage.png"),
-      bgImageSource: require("../../../assets/bg_7.png"),
+      bgImageSource: require("../../../assets/bg_7.jpeg"),
       name: "Rona Frishman |",
       exp: "Fearless",
       desp: "Ramat aliau, Rishon Le Zion, 20.01.2020 | 20:00",
